4721ed57a4803029e3abd3a1d55e4b45545ffa3c
Benoit Pierre <benoit.pierre@gmail.com>
2021-06-24 01:38:42 +0200

    use Plover 4.0.0.dev10 torture tests

--- test/test_digitalcat_dict.py
+++ test/test_digitalcat_dict.py
@@ -1,35 +1,40 @@
 # Copyright (c) 2017 Thomas Thurman
 # See LICENSE.txt for details.
 
-import unittest
+from pathlib import Path
 
-EXPECTED = {
-   ('TKPWHREPB',): 'glen',
-   ('A',): 'a',
-   ('TK*EB', 'TK*EB', 'R-R'):
-        # no, I don't know why; it was in the sample dct
-        '"Debbie \'Do Anything for Hillary\' Wasserman Schultz"',
-   ('TKEUGZ', 'AER'):
-        'dictionary',
-   ('HAOER',): 'here',
-   ('SEZ',): 'says',
-   ('S',): 'is',
-   ('SKW-FRPBLGTS',): '{^{A}And}',
-   ('STKPWHRAEPBG',): '{^{Q}What, if anything,^}',
-   ('SPHAUL',): 'small',
-}
+from plover_build_utils.testing import dictionary_test
 
 from plover_digitalcat_dictionary import DigitalCATDictionary
 
-class TestCase(unittest.TestCase):
 
-    def test_load_dictionary(self):
+@dictionary_test
+class TestDigitalCATDictionary:
 
-        # this test can produce useful output if it fails
-        self.maxDiff = None
+    DICT_CLASS = DigitalCATDictionary
+    DICT_EXTENSION = 'dct'
+    DICT_REGISTERED = True
+    DICT_LOAD_TESTS = (
+        lambda: (
+            'digitalcat-test.dct',
+            r'''
+            'TKPWHREPB': 'glen',
+            'A': 'a',
+            # no, I don't know why; it was in the sample dct
+            'TK*EB/TK*EB/R-R': '"Debbie \'Do Anything for Hillary\' Wasserman Schultz"',
+            'TKEUGZ/AER': 'dictionary',
+            'HAOER': 'here',
+            'SEZ': 'says',
+            'S': 'is',
+            'SKW-FRPBLGTS': '{^{A}And}',
+            'STKPWHRAEPBG': '{^{Q}What, if anything,^}',
+            'SPHAUL': 'small',
+            '''
+        ),
+    )
+    DICT_SAMPLE = 'digitalcat-test.dct'
 
-        filename = 'test/digitalcat-test.dct'
-        d = DigitalCATDictionary.load(filename)
-
-        for stroke, english in sorted(EXPECTED.items()):
-            self.assertEqual(d[stroke], english)
+    @staticmethod
+    def make_dict(contents):
+        path = Path(__file__).parent / contents
+        return path.read_bytes()
